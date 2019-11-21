4d03923f6552db6f2e5dae0cf007592dd60e3743
ThansksJava <luckyjiefeng@163.com>
2019-11-21 17:44:11 +0800

    python fifth day:multi thread

---
+++ muiltthread/distributedprocess/task_master.py
@@ -0,0 +1,39 @@
+import queue
+import random
+from multiprocessing.managers import BaseManager
+
+task_queue = queue.Queue()
+result_queue = queue.Queue()
+
+
+class QueueManager(BaseManager):
+    pass
+
+
+QueueManager.register("get_task_queue", callable=lambda :task_queue)
+QueueManager.register("get_result_queue", callable=lambda :result_queue)
+# bind port 5000,setup verification code
+manager = QueueManager(address=("127.0.0.1",5000),authkey=b"abc")
+# start Queue
+manager.start()
+# get the object visited by internet
+task = manager.get_task_queue()
+result = manager.get_result_queue()
+
+# put some tasks in in it
+for i in range(10):
+    n = random.randint(0, 1000)
+    print("Put task %d..." % n)
+    task.put(n)
+# get result from result queue
+print("try get result")
+for i in range(10):
+    r = result.get(timeout=10)
+    print("Result: %s" % r)
+# close manager
+manager.shutdown()
+print("master exit.")
+
+
+
+
---
+++ muiltthread/distributedprocess/task_worker.py
@@ -0,0 +1,34 @@
+import time,sys,queue
+from multiprocessing.managers import BaseManager
+
+SERVER_ADDR='192.168.253.1'
+
+
+class QueueManager(BaseManager):
+    pass
+
+
+QueueManager.register("get_task_queue")
+QueueManager.register("get_result_queue")
+# connect to the server endpoint
+server_addr = SERVER_ADDR
+print('Connect to server %s...' % server_addr)
+# port and verify code must conformity with the task_master.py file
+m = QueueManager(address=(server_addr, 5000), authkey=b'abc')
+# start connect
+m.connect()
+# get Queue object
+task = m.get_task_queue()
+result = m.get_result_queue()
+# get task from queue and record the result
+for i in range(10):
+    try:
+        n = task.get(timeout=1)
+        print('run task %d * %d...' % (n, n))
+        r = '%d * %d = %d' % (n, n, n*n)
+        time.sleep(1)
+        result.put(r)
+    except queue.Empty:
+        print('task queue is empty.')
+# 处理结束:
+print('worker exit.')
