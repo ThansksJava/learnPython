8c720cda7473a87eb15126ac59166ba4b5277468
fengjie <luckyjiefeng@163.com>
2019-11-26 17:48:08 +0800

    python seventh day:create app project

---
+++ awesome-webapp/www/app.py
@@ -0,0 +1,12 @@
+import logging; logging.basicConfig(level=logging.INFO)
+
+from aiohttp import web
+
+
+async def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+
+
+app = web.Application()
+app.add_routes([web.get('/', index)])
+web.run_app(app, host='127.0.0.1', port=9000)
