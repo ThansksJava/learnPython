b88dd1a1300d23c145c4d64adfee5aa78f383eb7
fengjie <luckyjiefeng@163.com>
2019-12-06 14:46:11 +0800

    create polls in django project

---
+++ djangodemo/README.md
@@ -0,0 +1,12 @@
+# django入门
+* django-admin startproject mysite
+* structure
+  ```
+        mysite/ --project container dir
+            manage.py --tools to manage django proj
+            mysite/  --project
+                __init__.py 
+                settings.py
+                urls.py --Django 项目的 URL 声明
+                wsgi.py --WSGI 兼容的Web服务器上的入口
+  ```
--- djangodemo/djangodemo/settings.py
+++ djangodemo/djangodemo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'djangodemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'python',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
